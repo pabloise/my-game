8cfa0d57bcdca83e0aacc62b876bb084567d293e
Pablo Bloise <pabloise10@gmail.com>
2020-11-10 11:07:09 -0300

    The food was added to the game and now everytime the two rectangles meet eachother in some point of the canvas, the score increases

--- js/game.js
+++ js/game.js
@@ -1,42 +1,71 @@
 var KEY_ENTER = 13,
-    KEY_LEFT = 37,
-    KEY_UP = 38,
-    KEY_RIGHT = 39,
-    KEY_DOWN = 40,
+KEY_LEFT = 37,
+KEY_UP = 38,
+KEY_RIGHT = 39,
+KEY_DOWN = 40,
 
-    canvas = null,
-    ctx = null,
-    lastPress = null,
-    pause = true,
-    x = 50,
-    y = 50,
-    dir = 0;
-
-window.requestAnimationFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 17);
-        };
-}());
+canvas = null,
+ctx = null,
+lastPress = null,
+pause = true,
+dir = 0,
+score = 0,
+player = null,
+food = null;
 
 document.addEventListener('keydown', function (evt) {
     lastPress = evt.which;
 }, false);
+
+function Rectangle(x, y, width, height) {
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+
+    this.intersects = function (rect) {
+        if (rect == null) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return (this.x < rect.x + rect.width &&
+                this.x + this.width > rect.x &&
+                this.y < rect.y + rect.height &&
+                this.y + this.height > rect.y);
+            }
+};
+
+this.fill = function (ctx) {
+    if (ctx == null) {
+        window.console.warn('Missing parameters on function fill');
+    } else {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
+
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
 
 function paint(ctx) {
     // Clean canvas
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Draw square
+    // Draw player
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(x, y, 10, 10);
+    player.fill(ctx);
+
+    // Draw food
+    ctx.fillStyle = '#f00';
+    food.fill(ctx);
 
     // Debug last key pressed
     ctx.fillStyle = '#fff';
-    //ctx.fillText('Last Press: ' + lastPress, 0, 20);
+    //ctx.fillText('Last Press: '+lastPress,0,20);
+
+    // Draw score
+    ctx.fillText('Score: ' + score, 0, 10);
 
     // Draw pause
     if (pause) {
@@ -45,77 +74,87 @@ function paint(ctx) {
         ctx.textAlign = 'left';
     }
 }
-
 function act() {
     if (!pause) {
         // Change Direction
         if (lastPress == KEY_UP) {
-            dir = 0;
+        dir = 0;
         }
         if (lastPress == KEY_RIGHT) {
-            dir = 1;
+        dir = 1;
         }
         if (lastPress == KEY_DOWN) {
-            dir = 2;
+        dir = 2;
         }
         if (lastPress == KEY_LEFT) {
-            dir = 3;
+        dir = 3;
         }
 
         // Move Rect
         if (dir == 0) {
-            y -= 10;
+        player.y -= 10;
         }
         if (dir == 1) {
-        x += 10;
+        player.x += 10;
         }
         if (dir == 2) {
-        y += 10;
+        player.y += 10;
         }
         if (dir == 3) {
-        x -= 10;
+        player.x -= 10;
         }
 
         // Out Screen
-        if (x > canvas.width) {
-        x = 0;
+        if (player.x > canvas.width) {
+        player.x = 0;
         }
-        if (y > canvas.height) {
-        y = 0;
+        if (player.y > canvas.height) {
+        player.y = 0;
         }
-        if (x < 0) {
-        x = canvas.width;
+        if (player.x < 0) {
+        player.x = canvas.width;
         }
-        if (y < 0) {
-        y = canvas.height;
+        if (player.y < 0) {
+        player.y = canvas.height;
+        }
+
+    // Food Intersects
+        if (player.intersects(food)) {
+        score += 1;
+        food.x = random(canvas.width / 10 - 1) * 10;
+        food.y = random(canvas.height / 10 - 1) * 10;
         }
 }
 
         // Pause/Unpause
-        if (lastPress == KEY_ENTER) {
-        pause = !pause;
-        lastPress = null;
-        }
+if (lastPress == KEY_ENTER) {
+    pause = !pause;
+    lastPress = null;
+    }
 }
-        function repaint() {
-            window.requestAnimationFrame(repaint);
-            paint(ctx);
-        }
 
-        function run() {
-        setTimeout(run, 50);
-        act();
-        }
+function repaint() {
+    window.requestAnimationFrame(repaint);
+    paint(ctx);
+}
 
-        function init() {
-            // Get canvas and context
-            canvas = document.getElementById('canvas');
-            ctx = canvas.getContext('2d');
+function run() {
+    setTimeout(run, 50);
+    act();
+}
 
-        // Start game
-        run();
-        repaint();
+function init() {
+    // Get canvas and context
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
-        }
+    // Create player and food
+    player = new Rectangle(40, 40, 10, 10);
+    food = new Rectangle(80, 80, 10, 10);
 
-        window.addEventListener('load', init, false);
+    // Start game
+    run();
+    repaint();
+    }
+
+window.addEventListener('load', init, false);
